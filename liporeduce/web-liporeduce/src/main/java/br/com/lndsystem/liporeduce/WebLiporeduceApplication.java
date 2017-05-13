4bd17c663476c425fb12ea76d6c9581a115427fb
leandro <leandro@192.168.0.34>
2017-05-13 18:52:41 -0300

    add projeto

---
+++ liporeduce/web-liporeduce/src/main/java/br/com/lndsystem/liporeduce/WebLiporeduceApplication.java
@@ -0,0 +1,12 @@
+package br.com.lndsystem.liporeduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLiporeduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLiporeduceApplication.class, args);
+	}
+}
