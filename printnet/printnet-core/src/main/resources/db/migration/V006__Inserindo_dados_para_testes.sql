748400fa583249785549300d34732aa35255ea65
leandro <leandro@192.168.0.33>
2017-07-10 06:57:59 -0300

    add

--- printnet/printnet-core/src/main/resources/db/migration/V006__Inserindo_dados_para_testes.sql
+++ printnet/printnet-core/src/main/resources/db/migration/V006__Inserindo_dados_para_testes.sql
@@ -1,4 +1,4 @@
-INSERT INTO processamento (id_requisicao,id_job,id_lote,id_cliente_global,id_sistema_global,id_status,ambiente,identificacao_externa,tipo_execucao,data_cadastro) VALUES 
+INSERT INTO [prodkit_inspire].[dbo].[processamento] (id_requisicao,id_job,id_lote,id_cliente_global,id_sistema_global,id_status,ambiente,identificacao_externa,tipo_execucao,data_cadastro) VALUES 
 	(1,123,456,789,469,7,'PRODUCAO','Exemplo.wfd','PRINTNET', GETDATE()),
 	(2,123,456,789,469,6,'PRODUCAO','Exemplo.wfd','PRINTNET', GETDATE()),
 	(3,123,456,789,469,5,'HOMOLOGACAO','Exemplo.wfd','PRINTNET', GETDATE()),
