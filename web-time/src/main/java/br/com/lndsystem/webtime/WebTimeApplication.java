4c1c873bad7831f118fbcd694eeef97d36e40f26
leandro <leandro@192.168.0.35>
2017-08-06 12:31:06 -0300

    add project

---
+++ web-time/src/main/java/br/com/lndsystem/webtime/WebTimeApplication.java
@@ -0,0 +1,12 @@
+package br.com.lndsystem.webtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTimeApplication.class, args);
+	}
+}
