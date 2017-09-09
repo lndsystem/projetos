836c6158bad47771c7bd3ee463a4e979e6dfba5c
Leandro Coelho Silva <leandro@MacBook-Pro-de-Leandro-2.local>
2017-09-08 22:27:57 -0300

    adicionando o projeto

---
+++ focoimovel/src/main/java/br/com/avlfocoimovel/FocoimovelApplication.java
@@ -0,0 +1,12 @@
+package br.com.avlfocoimovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocoimovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocoimovelApplication.class, args);
+	}
+}
