c93470dcc1c39cab9182d4b93d495a06e47de36f
leandro <leandro@192.168.10.35>
2018-02-02 22:12:27 -0200

    add

---
+++ pessoal/src/main/java/com/lndsystem/pessoal/PessoalApplication.java
@@ -0,0 +1,12 @@
+package com.lndsystem.pessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoalApplication.class, args);
+	}
+}
